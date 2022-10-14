0acafa8df8174769c27903d3beaa33b13dacc446
Nmait2111 <namitjain2111@gmail.com>
2022-10-14 11:50:16 +0530

    Final

--- Data_Changer.py
+++ Data_Changer.py
@@ -1,10 +1,9 @@
 student=[]
 def write(name):
-    print(name)
     name , r = name.split("_")
     if name not in student:
         student.append(name)
-        f = open('Frontend\\my-app\\src\\Dataforface.js','w')     
+        f = open('Dataforface.js','w')     
         f.write("const C = [\n"  )
         student.sort()
         for i in student:
---
+++ Dataforface.js
@@ -0,0 +1,5 @@
+const C = [
+
+{ Name : "Namit", RollNO : 2121382},
+{ Name : "Nishant", RollNO : 2121382},
+]; export default C
